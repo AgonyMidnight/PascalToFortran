6e8849fa193dca1c44f7f7e05a45a48deccfb6fa
AgonyMidnight <agony.midnight.am@gmail.com>
2020-10-20 14:33:25 +0300

    two

--- TyAP/MainWindow.xaml.cs
+++ TyAP/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,11 +21,38 @@ namespace TyAP
     /// </summary>
     public partial class MainWindow : Window
     {
+        Dictionary<string, string> dictionary = new Dictionary<string, string>();
         const int m1 = 19, m2 = 15;
         string [,] MyMatrix  = new string [m1,m2];
         public MainWindow()
         {
             InitializeComponent();
+            Begin(MyMatrix, dictionary);
+            label1.Content = MyMatrix[1, 1].ToString();
+        }
+        private void Begin(string [,] Matrix, Dictionary<string, string> dic)
+        {
+            StreamReader table = new StreamReader("table.txt");
+            StreamReader pairs = new StreamReader("encoding.txt");
+            string temp = "";
+            int str = 0;
+            while (temp != null)
+            {
+                temp = table.ReadLine();
+                string[] value = temp.Split(' ');
+                for (int i = 0; i < m2; i++)
+                {
+                    Matrix[str, i] = value[i];
+                }
+                str++;
+            }
+            temp = "";
+            while (temp != null)
+            {
+                temp = table.ReadLine();
+                string[] value = temp.Split(' ');
+                dic.Add(value[0], value[1]);
+            }
         }
     }
 }
