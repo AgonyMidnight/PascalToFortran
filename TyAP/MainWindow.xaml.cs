798667d0c303040bd60a453d6b7b915c6d4169c1
AgonyMidnight <agony.midnight.am@gmail.com>
2020-10-20 13:39:51 +0300

    to start

---
+++ TyAP/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TyAP
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        const int m1 = 19, m2 = 15;
+        string [,] MyMatrix  = new string [m1,m2];
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
